965fd08da4dd94b69c85366eff0e591b1a2af262
grozeser <wolfcc10@gmail.com>
2025-09-28 20:17:34 +0200

    Update main(1).cpp

--- lab1/main(1).cpp
+++ lab1/main(1).cpp
@@ -4,28 +4,28 @@
 //S = √p(p - a)(p - b)(p - c) - формула Герона
 //p = (a + b + c) / 2 - формула полупериметра
 
-int main() 
+int main()
 {
+
+    setlocale(LC_ALL, "Russian");
+
+    int a, b, c, r1, r2;
+    r1 = sizeof(int) * 8;
+    std::cin >> a >> b >> c;
     
-    int a;     //челое число (сторона треугольника)
-    int b;     //челое число (сторона треугольника)
-    int c;     //челое число (сторона треугольника)
+    double p, S; 
+    r2 = sizeof(double) * 8;
+   
 
-    float p;   //полупериметр(может быть дробным (макс. ... . 5) поэтому хватит float)
-    double S;  //S может получиться дробным
-
-    a = 10000
-    b = 55555
-    c = 25324
-
-
-    p = (a + b + c) / 2 
-
-    S = sqrt(p(p - a)(p - b)(p - c))
+    p = (a + b + c) / 2.0;
+    S = std::sqrt(p * (p - a) * (p - b) * (p - c));
+     
+    std::cout << " int занимает " << r1 << "битов. Min=" << INT_MIN << "Max=" << INT_MAX << std::endl;
+    std::cout << " double занимает " << r2 << "битов. Min=" << DBL_MIN << "Max=" << DBL_MAX << std::endl;
+    
 
     std::cout << S << std::endl;
 
 
     return 0;
 }
-
